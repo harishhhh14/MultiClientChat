4d82614060cef29ec39094e86ff441495fcae53f
Harish <harishanbuvel14@gmail.com>
2026-03-29 13:19:29 +0530

    Add multithreading support for client handling

--- server.c
+++ server.c
@@ -10,30 +10,57 @@
 
 int clients[MAX_CLIENTS];
 int client_count = 0;
+pthread_mutex_t lock;
 
 void broadcast(char *message, int sender_sock) {
+    pthread_mutex_lock(&lock);
+
     for(int i = 0; i < client_count; i++) {
         if(clients[i] != sender_sock) {
             send(clients[i], message, strlen(message), 0);
         }
     }
+
+    pthread_mutex_unlock(&lock);
 }
 
-void* handle_client(void* socket_desc) {
-    int sock = *(int*)socket_desc;
+void* server_send(void* arg) {
+    char message[1024];
+
+    while(1) {
+        fgets(message, sizeof(message), stdin);
+
+        pthread_mutex_lock(&lock);
+
+        for(int i = 0; i < client_count; i++) {
+            send(clients[i], message, strlen(message), 0);
+        }
+
+        pthread_mutex_unlock(&lock);
+    }
+
+    return NULL;
+}
+
+
+void* handle_client(void* arg) {
+    int sock = *(int*)arg;
+    free(arg);
+
     char buffer[1024];
 
     while(1) {
         memset(buffer, 0, sizeof(buffer));
-        int read_size = recv(sock, buffer, 1024, 0);
 
-        if(read_size <= 0) {
+        int bytes = recv(sock, buffer, sizeof(buffer), 0);
+
+        if(bytes <= 0) {
             printf("Client disconnected\n");
             close(sock);
             break;
         }
 
-        printf("Client: %s\n", buffer);
+        printf("Client: %s", buffer);
 
         broadcast(buffer, sock);
     }
@@ -46,26 +73,44 @@ int main() {
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
+    pthread_mutex_init(&lock, NULL);
+
+
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    printf("Socket created\n");
+
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, 5);
+    
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    printf("Bind successful\n");
 
-    printf("Server started...\n");
+   
+    listen(server_fd, 5);
+    printf("Server listening...\n");
+
+    pthread_t send_thread;
+    pthread_create(&send_thread, NULL, server_send, NULL);
 
     while(1) {
-        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-
+        new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
         printf("New client connected\n");
 
+        pthread_mutex_lock(&lock);
         clients[client_count++] = new_socket;
+        pthread_mutex_unlock(&lock);
 
-        pthread_t thread_id;
-        pthread_create(&thread_id, NULL, handle_client, (void*)&new_socket);
+        int *new_sock = malloc(sizeof(int));
+        *new_sock = new_socket;
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, handle_client, (void*)new_sock);
     }
 
     return 0;
